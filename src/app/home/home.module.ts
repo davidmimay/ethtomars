25eab0ed3fa991751e4ff5fb11609fe14183bdc0
david mimay <davidmimay@gmail.com>
2021-12-29 19:21:44 +0000

    👉 affiliates

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,13 @@ import { SharedModule } from './../shared/shared.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AffiliateComponent } from './affiliate/affiliate.component';
 
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [
+    MainPageComponent,
+    AffiliateComponent],
   imports: [
     CommonModule,
     SharedModule,
